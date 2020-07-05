6ecd8a875b0cbfd3bf90425610e91ffe1ac1b58e
dyang504 <dengyang92@qq.com>
2020-07-05 15:01:06 +0800

    Updates

---
+++ precache-manifest.b9ffab1ea3633bd423e687f1a7f1bb4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6983b8a2e89a98d1d04bc338b255e93f",
+    "url": "/iata_code_helper/index.html"
+  },
+  {
+    "revision": "2080ba8208891efbea6c",
+    "url": "/iata_code_helper/static/css/main.f9eb5fd0.chunk.css"
+  },
+  {
+    "revision": "39ec6384ca6e71cc6841",
+    "url": "/iata_code_helper/static/js/2.11efe25b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iata_code_helper/static/js/2.11efe25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2080ba8208891efbea6c",
+    "url": "/iata_code_helper/static/js/main.bf2dada6.chunk.js"
+  },
+  {
+    "revision": "c0fec5f83dd42c60c87a",
+    "url": "/iata_code_helper/static/js/runtime-main.5705d233.js"
+  }
+]);
